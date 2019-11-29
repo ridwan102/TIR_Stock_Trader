76a1b8ee04c5234c9f6f1639bc6a1880918497d6
ridwan102 <ubuntu@ide.ide.6b7e806c-e075-4a29-8da3-85aa9a7f3090.svc.cluster.local>
2019-11-29 21:42:26 +0000

    first reboot of cs50finance

---
+++ application.py
@@ -0,0 +1,361 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    #set up api key for the stock quote engine
+    try:
+        os.environ["API_KEY"] = "pk_0d1467f2aeb748d0b81f445f22bd6665"
+    except:
+        raise RuntimeError("API_KEY not set")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+
+        # Enter valid username
+        if not request.form.get("username"):
+            flash("Please enter a username")
+            return redirect("/register")
+
+        # Enter valid password
+        elif not request.form.get("password"):
+            flash("Please enter a password")
+            return redirect("/register")
+
+        # Ensure confirmation password was submitted
+        elif not request.form.get("confirmation"):
+            flash("Please confirm password")
+            return redirect("/register")
+
+        # Ensure passwords match
+        elif request.form.get("password") != request.form.get("confirmation"):
+            flash("Passwords do not match")
+            return redirect("/register")
+
+        # Ensure username is not taken
+        elif len(rows) == 1:
+            flash("Username is already taken")
+            return redirect("/register")
+
+        # saves password to hash
+        hash = generate_password_hash(request.form.get("password"))
+
+        # adds username to form
+        new_user_id = db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)",
+                                 username=request.form.get("username"), hash=hash)
+
+        # Save user id into session
+        session["user_id"] = new_user_id
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    # session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("Please provide username")
+            return redirect("/login")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("Please provide password")
+            return redirect("/login")
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            flash("Invalid username/password")
+            return redirect("/login")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+
+    # calls current user
+    user = db.execute("SELECT username FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["username"]
+
+    # deletes rows with no stocks
+    db.execute("DELETE FROM buy WHERE shares = 0")
+
+    # updates total in dollar value amount of bought stocks
+    db.execute("UPDATE buy SET total=price*shares")
+
+    # initializes buy table for current user
+    stocks = db.execute("SELECT * FROM buy WHERE user = :user", user=user)
+
+    # totals all stock investments for current user
+    stock_total = db.execute("SELECT SUM(total) FROM buy WHERE user = :user", user=user)
+
+    # calls amount of cash user currently has on hand
+    cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])
+
+    # check for new users that do not have any investments or else there is an error
+    if not stock_total[0]['SUM(total)']:
+        stock_totals = 0
+    else:
+        stock_totals = stock_total[0]['SUM(total)']
+
+    # calculators remainder amount of cash
+    remainder = cash[0]['cash'] - stock_totals
+
+    # updates remainder amount of cash
+    db.execute("UPDATE users SET remainder = :remainder WHERE username=:username", remainder=remainder, username=user)
+
+    # calculats total of remainder and total investments
+    total = remainder + stock_totals
+
+    return render_template("index.html", stocks=stocks, stock_totals=stock_totals, remainder=remainder, total=total)
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    # User reached route via POST (as by submitting a form via POST)
+
+    if request.method == "POST":
+        symbol = lookup(request.form.get("symbol"))
+
+        if not symbol:
+            flash("Does not exist")
+            return redirect("/quote")
+
+        flash(f"{symbol['name']} is ${symbol['price']} per share")
+        return redirect("/quote")
+
+    else:
+        return render_template("quote.html")
+
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # calls which stock symbol user input
+        symbol = lookup(request.form.get("symbol"))
+
+        # if the user enters an incorrect symbol, they are prompted
+        if not symbol:
+            flash("Does not exist")
+            return redirect("/buy")
+
+        # it initially sees if a valid number of shares is entered
+        try:
+            shares = int(request.form.get("shares"))
+        except:
+            flash("Please enter a valid amount of shares to buy")
+            return redirect("/buy")
+
+        if shares <= 0:
+            flash("Please enter a number greater than 0", 400)
+            return redirect("/buy")
+
+        # calls remainder cash on account
+        remainder = db.execute("SELECT remainder FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["remainder"]
+
+        # calls current price of stock
+        price = symbol["price"]
+
+        # if desired total to be bought is greater than remaining cash, flashes error message
+        if price * shares > remainder:
+            flash("Not enough funds available")
+            return redirect("/buy")
+
+        # calls current user
+        user = db.execute("SELECT username FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["username"]
+
+        # calls amount of current of shares for user
+        user_shares = db.execute("SELECT shares FROM buy WHERE user = :user AND stock=:stock",
+                                 user=user, stock=symbol["name"])
+
+        # if the current stock is not in the users profile, it inserts it or else it updates the table
+        if not user_shares:
+            db.execute("INSERT INTO buy (user, stock, symbol, price, shares) VALUES(:user, :stock, :symbol, :price, :shares)",
+                       user=user, stock=symbol["name"], symbol=symbol["symbol"], price=price, shares=shares)
+        else:
+            db.execute("UPDATE buy SET shares = shares + :shares WHERE user=:user AND stock = :stock",
+                       shares=shares, user=user, stock=symbol["name"])
+
+        # inserts transaction into history
+        db.execute("INSERT INTO history (user, buysell, stock, price, shares) VALUES(:user, 'Buy', :stock, :price, :shares)",
+                   user=user, stock=symbol["name"], price=price, shares=shares)
+
+        flash(f"Purchased {shares} shares of {symbol['name']}")
+
+        return redirect("/")
+
+    else:
+        return render_template("buy.html")
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
+
+    # initializes to current user
+    user = db.execute("SELECT username FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["username"]
+
+    # initializes symbol for buy table for current user
+    symbols = db.execute("SELECT symbol FROM buy WHERE user = :user", user=user)
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # initializes symbol for buy table for current user
+        symbol = symbols[0]["symbol"]
+
+        # if share value to sell is not entered or shares are equal or less than 0, returns apology
+        try:
+            shares = int(request.form.get("shares"))
+        except:
+            flash("Please enter a valid amount of shares to sell")
+            return redirect("/sell")
+
+        if shares <= 0:
+            flash("Please enter a valid amount of shares to sell")
+            return redirect("/sell")
+
+        # calls total shares for current user and stock
+        total_shares = db.execute("SELECT shares FROM buy WHERE user=:user AND symbol = :symbol",
+                                  user=user, symbol=symbol)[0]["shares"]
+
+        # if shares requested to sell is greater than total shares, returns error
+        if shares > total_shares:
+            flash("Not enough shares")
+            return redirect("/sell")
+
+        # looks up stock price based on symbol
+        price = lookup(symbol)["price"]
+
+        # calls current stock
+        stock = db.execute("SELECT stock FROM buy WHERE user=:user AND symbol = :symbol", user=user, symbol=symbol)[0]["stock"]
+
+        # updates shares for selected stock
+        db.execute("UPDATE buy SET shares = shares - :shares WHERE user=:user AND symbol = :symbol", shares=shares, user=user,
+                   symbol=symbol)
+
+        # add transaction to history
+        db.execute("INSERT INTO history (user, buysell, stock, price, shares) VALUES(:user, 'Sell', :stock, :price, :shares)",
+                   user=user, stock=stock, price=price, shares=shares)
+
+        flash(f"Sold {shares} shares of {stock}")
+
+        return redirect("/")
+
+    else:
+
+        # User reached route via GET (as by clicking a link or via redirect)
+        return render_template("sell.html", symbols=symbols)
+
+
+@app.route("/history")
+@login_required
+def history():
+
+    # calls current user
+    user = db.execute("SELECT username FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["username"]
+
+    # initializes history table for current user
+    stocks = db.execute("SELECT * FROM history WHERE user = :user", user=user)
+
+    return render_template("history.html", stocks=stocks)
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return (e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
---
+++ helpers.py
@@ -0,0 +1,50 @@
+import os
+import requests
+import urllib.parse
+import apikeys
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def lookup(symbol):
+    """Look up quote for symbol."""
+    os.system("export API_KEY="+apikeys.APIKEYvalue)
+
+    # Contact API
+    try:
+        api_key = os.environ.get("API_KEY")
+        response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    # Parse response
+    try:
+        quote = response.json()
+        return {
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"]
+        }
+    except (KeyError, TypeError, ValueError):
+        return None
+
+
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
