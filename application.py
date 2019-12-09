0e518b6ab076d2ee4811e80d5ec83ee013376048
Ridwan Alam <ridwan102@gmail.com>
2019-12-09 13:50:00 -0500

    adjusted register page

--- application.py
+++ application.py
@@ -129,10 +129,6 @@ def register():
         # Redirect user to home page
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("register.html")
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
