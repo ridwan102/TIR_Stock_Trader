ce0144981fc4a4d1ecac5c6d5b2b3fb9020a948e
Ridwan Alam <ridwan102@gmail.com>
2020-02-13 01:00:15 -0500

    added session cookie domain

--- application.py
+++ application.py
@@ -64,6 +64,7 @@ app.jinja_env.filters["usd"] = usd
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_COOKIE_DOMAIN"] = '127.0.0.1'
 Session(app)
 
 # Make sure API key is set
