d8c9c400041a9c7e123334b0afb685b2d92299ab
Ridwan Alam <ridwan102@gmail.com>
2020-02-12 19:23:38 -0500

    changed f in helper

--- helpers.py
+++ helpers.py
@@ -6,17 +6,17 @@ from flask import redirect, render_template, request, session
 from functools import wraps
 
 
-def login_required(f):
+def login_required():
     """
     Decorate routes to require login.
 
     http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
     """
-    @wraps(f)
+    @wraps()
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/")
-        return f(*args, **kwargs)
+        return (*args, **kwargs)
     return decorated_function
 
 
