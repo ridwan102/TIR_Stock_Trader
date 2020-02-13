e8bf5f53e5d233c9af9d29dd56fc266d79efaa86
Ridwan Alam <ridwan102@gmail.com>
2020-02-13 13:01:56 -0500

    commented out boto

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
-from boto.s3.connection import S3Connection
+#from boto.s3.connection import S3Connection
 
 from helpers import login_required, lookup, usd
 
