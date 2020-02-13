8d89c93992071c3e926f812449287442fbd67886
Ridwan Alam <ridwan102@gmail.com>
2020-02-13 13:38:50 -0500

    removed login required

--- helpers.py
+++ helpers.py
@@ -3,22 +3,22 @@ import requests
 import urllib.parse
 
 from flask import redirect, render_template, request, session, url_for
-from functools import wraps
+#from functools import wraps
 
 
-def login_required(f):
-    """
+""" def login_required(f):
+    
     Decorate routes to require login.
 
     http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
-    """
+    
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             #changed from login page to index 
             return redirect(url_for('index'))
         return f(*args, **kwargs)
-    return decorated_function
+    return decorated_function """
 
 
 # allows user to call stocks based on symbol
