cbe74b25081588e921e31fa8deb84b8bea606c6e
Ridwan Alam <ridwan102@gmail.com>
2020-02-13 13:13:46 -0500

    changed redirect to index

--- application.py
+++ application.py
@@ -21,8 +21,36 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 # calls SQLAlchemy database
 db = SQLAlchemy(app)
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    #set up api key for the stock quote engine
+    try:
+        os.environ["API_KEY"] = "pk_0d1467f2aeb748d0b81f445f22bd6665"
+    except:
+        raise RuntimeError("API_KEY not set")
 
 # defines table values
 class User(db.Model):
@@ -49,34 +77,6 @@ class History(db.Model):
     stocksymbol = db.Column(db.String(80), nullable=False)
     stockprice = db.Column(db.Float(7), nullable=False)
     stockshares = db.Column(db.Float(7), nullable=False)
-
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Make sure API key is set
-if not os.environ.get("API_KEY"):
-    #set up api key for the stock quote engine
-    try:
-        os.environ["API_KEY"] = "pk_0d1467f2aeb748d0b81f445f22bd6665"
-    except:
-        raise RuntimeError("API_KEY not set")
 
 
 @app.route("/register", methods=["GET", "POST"])
