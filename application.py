8957a35d94741fceb479d7e4aecf87ceb06334f7
Ridwan Alam <ridwan102@gmail.com>
2020-02-13 13:31:56 -0500

    changed

--- application.py
+++ application.py
@@ -101,27 +101,27 @@ def register():
         # Enter valid username
         if not username:
             flash("Please enter a username")
-            #return redirect("/register")
+            return redirect("/register")
 
         # Enter valid password
         elif not password:
             flash("Please enter a password")
-            #return redirect("/register")
+            return redirect("/register")
 
         # Password was not confirmed
         elif not confirmation:
             flash("Please confirm password")
-            #return redirect("/register")
+            return redirect("/register")
 
         # Ensure passwords match
         elif password != confirmation:
             flash("Passwords do not match")
-            #return redirect("/register")
+            return redirect("/register")
 
         # if username is taken
         elif user:
             flash("Username is already taken")
-            #return redirect("/register")
+            return redirect("/register")
 
         # saves password to hash
         password = generate_password_hash(password)
