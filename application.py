91578926e3cefc03c94f438032d5c9eaada079cc
Ridwan Alam <ridwan102@gmail.com>
2020-03-03 11:52:08 -0500

    changed session_ermanent to true

--- application.py
+++ application.py
@@ -29,8 +29,8 @@ app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "sqlalchemy"
+app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem"
 
 sess = Session()
 
